99e462172bcd264f10d46be69dd9db26563ddcc3
kayiwarahim <kayiwarahim@gmail.com>
2025-07-02 05:59:11 +0300

    Initialize agent.py and requirements.txt files

---
+++ persoonal_X_AI_agent/agent.py
@@ -0,0 +1,54 @@
+import os
+import tweepy
+from dotenv import load_dotenv
+import schedule
+import time
+
+# Load credentials from .env file
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+API_KEY_SECRET = os.getenv('API_KEY_SECRET')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+BEARER_TOKEN = os.getenv('BEARER_TOKEN')
+
+# Create Tweepy Client for v2
+client = tweepy.Client(
+    bearer_token=BEARER_TOKEN,
+    consumer_key=API_KEY,
+    consumer_secret=API_KEY_SECRET,
+    access_token=ACCESS_TOKEN,
+    access_token_secret=ACCESS_TOKEN_SECRET
+)
+
+# List of tweets to post
+tweets = [
+    "Hello world! #RahimAgentAI",
+    "This is an automated post. #XAPI",
+    "Posting from my Python bot every 2 minutes!",
+    "Follow me for more updates! #Bot"
+]
+tweet_index = 0
+
+def post_tweet(message):
+    try:
+        response = client.create_tweet(text=message)
+        print(f'Tweet posted successfully! Tweet ID: {response.data["id"]}')
+    except Exception as e:
+        print(f'Error posting tweet: {e}')
+
+def job():
+    global tweet_index
+    if tweet_index < len(tweets):
+        post_tweet(tweets[tweet_index])
+        tweet_index += 1
+    else:
+        print("All tweets have been posted. Stopping the bot.")
+        exit()
+
+if __name__ == '__main__':
+    schedule.every(2).minutes.do(job)
+    print("Bot started. Posting every 2 minutes.")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
